373cb281ab0d687f237012a3a15a5e45c86129ac
R. Tyler Julian <trjulian@gmail.com>
2025-05-04 17:03:04 -0400

    piece movement

--- src/components/board/Editor.tsx
+++ src/components/board/Editor.tsx
@@ -29,6 +29,7 @@ export function Editor() {
     "MOVE" | "TRASH" | "PLACE"
   >("MOVE");
   const [selectedFrom, setSelectedFrom] = useState<Coordinate | null>(null);
+  const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
 
   const board = defaultBoard;
   const redReserve = defaultReserveFleet;
@@ -56,16 +57,23 @@ export function Editor() {
       } else if (selectedAction === "MOVE") {
         if (isMouseDown) {
           setSelectedFrom([rowIndex, colIndex]);
-        } else if (selectedFrom) {
-          const square = board[selectedFrom[0]][selectedFrom[1]];
-          if (square) {
-            board[selectedFrom[0]][selectedFrom[1]] = null;
-            board[rowIndex][colIndex] = square;
-          }
+          setSelectedSquare(board[rowIndex][colIndex]);
+        } else if (selectedFrom && selectedSquare) {
+          board[selectedFrom[0]][selectedFrom[1]] = null;
+          board[rowIndex][colIndex] = selectedSquare;
+          setSelectedFrom(null);
+          setSelectedSquare(null);
         }
       }
     },
-    [board, selectedReserve, selectedPlayer, selectedAction, selectedFrom]
+    [
+      board,
+      selectedReserve,
+      selectedPlayer,
+      selectedAction,
+      selectedFrom,
+      selectedSquare,
+    ]
   );
 
   const handleMouseOver = () => {};
@@ -124,7 +132,12 @@ export function Editor() {
                 <SquareComponent
                   squareSize={squareSize}
                   pieceSize={pieceSize}
-                  squareState={nullSquareState(rowIndex, colIndex, square)}
+                  squareState={simpleSquareState(
+                    rowIndex,
+                    colIndex,
+                    square,
+                    selectedFrom
+                  )}
                   isFlipped={false}
                 />
               </div>
@@ -132,39 +145,50 @@ export function Editor() {
           </div>
         ))}
       </BoardContainer>
-      {(selectedAction === "PLACE" || selectedAction === "TRASH") && (
-        <div
-          className="fixed pointer-events-none z-50"
-          style={{
-            width: pieceSize * 0.7,
-            height: pieceSize * 0.7,
-            left: mousePosition.x,
-            top: mousePosition.y,
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          {selectedAction === "PLACE" && selectedReserve && (
-            <img
-              className={cn(selectedPlayer === "BLUE" && "rotate-180")}
-              src={`/${Units[selectedReserve].imagePathPrefix}-${selectedPlayer}.png`}
-              width={pieceSize * 0.7}
-              height={pieceSize * 0.7}
-              draggable="false"
-              alt={Units[selectedReserve].imagePathPrefix}
-            />
-          )}
-          {selectedAction === "TRASH" && (
-            <img
-              className="bg-white/80 rounded-lg p-0.5"
-              src={`trash-2.svg`}
-              width={pieceSize * 0.7}
-              height={pieceSize * 0.7}
-              draggable="false"
-              alt="trash"
-            />
-          )}
-        </div>
-      )}
+
+      <div
+        className="fixed pointer-events-none z-50"
+        style={{
+          width: pieceSize * 0.7,
+          height: pieceSize * 0.7,
+          left: mousePosition.x,
+          top: mousePosition.y,
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        {selectedAction === "MOVE" && selectedSquare && (
+          <img
+            className={cn(selectedSquare.player === "BLUE" && "rotate-180")}
+            src={`/${Units[selectedSquare.type].imagePathPrefix}-${
+              selectedSquare.player
+            }.png`}
+            width={pieceSize * 0.7}
+            height={pieceSize * 0.7}
+            draggable="false"
+            alt={Units[selectedSquare.type].imagePathPrefix}
+          />
+        )}
+        {selectedAction === "PLACE" && selectedReserve && (
+          <img
+            className={cn(selectedPlayer === "BLUE" && "rotate-180")}
+            src={`/${Units[selectedReserve].imagePathPrefix}-${selectedPlayer}.png`}
+            width={pieceSize * 0.7}
+            height={pieceSize * 0.7}
+            draggable="false"
+            alt={Units[selectedReserve].imagePathPrefix}
+          />
+        )}
+        {selectedAction === "TRASH" && (
+          <img
+            className="bg-white/80 rounded-lg p-0.5"
+            src={`trash-2.svg`}
+            width={pieceSize * 0.7}
+            height={pieceSize * 0.7}
+            draggable="false"
+            alt="trash"
+          />
+        )}
+      </div>
 
       <div className="flex items-center justify-center gap-1">
         <ReserveBankButton
@@ -224,10 +248,11 @@ function useBoardDimensions() {
 
   return { measureRef, squareSize, pieceSize };
 }
-function nullSquareState(
+function simpleSquareState(
   rowIndex: number,
   colIndex: number,
-  square: Square
+  square: Square,
+  selectedFrom: Coordinate | null
 ): SquareState {
   return {
     rowIndex,
@@ -245,7 +270,11 @@ function nullSquareState(
     isMovable: false,
     isRightClicked: false,
     isHovered: false,
-    isMidMove: false,
+    isMidMove:
+      (selectedFrom &&
+        selectedFrom[0] === rowIndex &&
+        selectedFrom[1] === colIndex) ||
+      false,
     shouldAnimateTo: undefined,
     engagedOrientation: undefined,
   };
